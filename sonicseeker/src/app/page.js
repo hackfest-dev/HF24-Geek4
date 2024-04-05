1095eb1de7b1d32642264d4ccedebfa346c9cda1
awesomecode <ankithh354@gmail.com>
2024-04-05 19:06:35 +0530

    Error Push

--- sonicseeker/src/app/page.js
+++ sonicseeker/src/app/page.js
@@ -3,7 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      {/* Add your desired content here */}
+      <div>
+        <h1>This is Our Big project</h1>
+        
+      </div>
     </main>
   );
 }
