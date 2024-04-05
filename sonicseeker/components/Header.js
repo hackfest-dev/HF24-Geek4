fd015233032bad47666e324bd97c899c210d90ab
awesomecode <ankithh354@gmail.com>
2024-04-05 20:40:06 +0530

    Added Header and Footer

--- sonicseeker/components/Header.js
+++ sonicseeker/components/Header.js
@@ -1,22 +1,8 @@
-<<<<<<< HEAD
 import Link from "next/link";
 import styles from "./Header.module.css";
-=======
-"use client" 
-import React, { useState } from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
->>>>>>> f2279871a600cacb55f39667a3f9dcaf49622fc3
 
-const Navbar = () => {
-       const [isClick,setisClick] = useState(false);
-
-       const toggleNavbar = () => {
-
-        setisClick(!isClick);
-       }
+const Header = () => {
   return (
-<<<<<<< HEAD
     <header className={styles.wrapper}>
       <Link href="/">
         <div className={styles.logo}>
@@ -35,42 +21,7 @@ const Navbar = () => {
         </div>
       </Link>
     </header>
-=======
-    <nav className='bg-black'>
-      <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
-             <div className='flex items-center justify-between h-16'>
-                  <div className='flex items-center'>
-                    <div className='flex-shrink-0'>
-                      <a href = "/" className='text-white'>
-                        Logo
-                      </a>
-                    </div>
-                  </div>
-                  <div className='hidden md:block'>
-                    <div className='ml-4 flex items-center space-x-4'>
-                        <a href = "/" className='text-white hover:bg-white hover:text-black rounded-lg p-2'> Home
-                        
-                        </a>
-                         <a href = "/" className='text-white hover:bg-white hover:text-black rounded-lg p-2'> Services
-                        
-                        </a>
-                         <a href = "/" className='text-white hover:bg-white hover:text-black rounded-lg p-2'> Contact
-                        
-                        </a>
-                         <a href = "/" className='text-white hover:bg-white hover:text-black rounded-lg p-2'> About
-                        
-                        </a>
-                    </div>
-                  </div>
-                  <div className='md:hidden flex items-center'>
-                    <button className='inline-flex items-center justify-center p-2 rounded-md text-white md:text-white hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white ' onClick={toggleNavbar}/>
-
-                  </div>
-                   </div>
-             </div>
-    </nav>
->>>>>>> f2279871a600cacb55f39667a3f9dcaf49622fc3
   );
 };
 
-export default Navbar;
+export default Header;
--- sonicseeker/src/app/page.js
+++ sonicseeker/src/app/page.js
@@ -1,31 +1,15 @@
-<<<<<<< HEAD
-=======
-
 import Footer from "../../components/Footer";
->>>>>>> f2279871a600cacb55f39667a3f9dcaf49622fc3
 import Header from "../../components/Header";
 
 export default function Home() {
   return (
-<<<<<<< HEAD
     <div className="flex flex-col min-h-screen">
       <Header />
+      <Footer/>
       <main className="flex flex-1 items-center justify-center">
         {/* Your main content here */}
         <h1 className="text-4xl font-bold">Welcome to the homepage</h1>
       </main>
     </div>
-=======
-    
-      
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      
-       
-        
-    <Header />
-    <Footer />
-
-    </main>
->>>>>>> f2279871a600cacb55f39667a3f9dcaf49622fc3
   );
 }
