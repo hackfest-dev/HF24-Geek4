f2f810f30400af7283869cf3729512227e3f83d3
Nandan Upadhyaya <nandanupadhyaya18@gmail.com>
2024-04-05 18:54:59 +0530

    Header js

--- sonicseeker/components/Header.js
+++ sonicseeker/components/Header.js
@@ -2,29 +2,28 @@ import Link from "next/link";
 import Router from "next/router";
 
 
-
 const Header = () => {
   return (
-    <header className=" grid min-h-50  ">
+    <header className="grid min-h-50 grid-cols-12 bg-[#21212c] pt-3 pb-3 pl-20 pr-20 text-[#FBC1C1] mb-5">
       <Link href="/">
         <div
-          className={styles.logo}
+          className="grid grid-cols-12 cursor-pointer"
           onClick={() => {
             window.location.pathname === "/" &&
               Router.reload(window.location.pathname);
           }}
         >
-          <img className={styles.icon} src="logo/cue.png" alt="cue" />
+          <img className="w-8 h-8" src="logo/cue.png" alt="cue" />
           <h2 className={styles.name}>VoiceCue</h2>
         </div>
       </Link>
       <Link href="/features">
-        <div className={styles.navItem}>
+        <div className="grid items-end">
           <p className={`${styles.link} ${styles.page}`}>Features</p>
         </div>
       </Link>
       <Link href="/about">
-        <div className={styles.navItem}>
+        <div className="grid items-end">
           <p className={`${styles.link} ${styles.page}`}>About</p>
         </div>
       </Link>
