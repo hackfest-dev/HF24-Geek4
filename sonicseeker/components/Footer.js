6cce1a6d204279edd02b72ecd46c029a36678f6e
Krishna N Acharya <knacharyakavoor@gmail.com>
2024-04-05 18:32:24 +0530

    remade footer

---
+++ sonicseeker/components/Footer.js
@@ -0,0 +1,42 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.wrapper}>
+      <p className={styles.author}>
+        Made by a humble NITTE team{" "}
+        <span className={styles.link}>
+          <a
+            href="https://github.com/hackfest-dev/HF24-Geek4"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Geek4
+          </a>
+        </span>
+      </p>
+      <p>
+        Powered by{" "}
+        <span className={styles.link}>
+          <a href="https://deepgram.com" target="_blank" rel="noreferrer">
+            Deepgram
+          </a>
+        </span>
+        ,{" "}
+        <span className={styles.link}>
+          <a href="https://nextjs.org" target="_blank" rel="noreferrer">
+            NextJS
+          </a>
+        </span>{" "}
+        and{" "}
+        <span className={styles.link}>
+          <a href="https://vercel.com" target="_blank" rel="noreferrer">
+            Vercel
+          </a>
+        </span>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
